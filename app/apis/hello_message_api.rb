bca747f2627caa0013cc348d8dfa4524d34d7b45
Codingmart <codingmart@Codingmarts-MacBook-Air.local>
2021-02-15 13:41:45 +0530

    initial commit

---
+++ app/apis/hello_message_api.rb
@@ -0,0 +1,4 @@
+class HelloMessageApi < ActionWebService::API::Base
+  api_method :hello_message, :expects => [{:firstname=>:string}, 
+{:lastname=>:string}], :returns => [:string]
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+	get 'hello_message/wsdl', to: 'hello_message#hello_message'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
