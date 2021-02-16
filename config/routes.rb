3ac2c7e257178d295e55f379da38f51ecbb43f36
Codingmart <codingmart@Codingmarts-MacBook-Air.local>
2021-02-16 15:09:18 +0530

    xml implimentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	get 'hello_message/wsdl', to: 'hello_message#hello_message'
+	get '/getdata', to: 'rumbas#generate_object'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   wash_out :rumbas
